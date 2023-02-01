71212c1e8796ee452f4ca181e1dff91d3ed51adb
Qihua <zhongqihua@gmail.com>
2023-02-01 05:15:50 -0500

    Support for multiprocessing

--- train_td3.py
+++ train_td3.py
@@ -9,7 +9,7 @@ from hge import HgeTD3
 from utils.callbacks import HgeRateCallback, SaveEnvStatsCallback, HParamCallback
 import gym
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.vec_env import VecNormalize, SubprocVecEnv
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.callbacks import EvalCallback
 import time
@@ -78,12 +78,11 @@ def main():
         rule=args.ordering_rule,
     )
 
-    # Register different versions of the beer game to the Gym Registry, so the environment can be created using gym.make
-    register_envs()
-
     n_env = 8
 
     def env_factory() -> gym.Env:
+        # Register different versions of the beer game to the Gym Registry, so the environment can be created using gym.make
+        register_envs()
         if args.ordering_rule == "d+a":
             return wrap_action_d_plus_a(
                 gym.make(env_name),
@@ -99,7 +98,7 @@ def main():
     for run in range(setup["runs"]):
 
         exp_name = f"{args.name}_TD3_{args.role}_{args.scenario}_{'FullInfo'*args.global_info}_{args.ordering_rule}_{run}_{time.time_ns()}"
-        env = VecNormalize(make_vec_env(env_factory, n_env), clip_obs=100, clip_reward=1000)
+        env = VecNormalize(make_vec_env(env_factory, n_env, vec_env_cls=SubprocVecEnv), clip_obs=100, clip_reward=1000)
         # eval_env = VecNormalize(make_vec_env(env_factory, n_env), clip_obs=100, clip_reward=1000)
 
         policy_kwargs = dict(net_arch=[params["network_width"]] * params["num_layers"])
