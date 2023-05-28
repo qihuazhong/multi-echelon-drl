3322e88a9f8450339771a49c12129256009f645e
Qihua <zhongqihua@gmail.com>
2023-05-28 01:07:35 -0400

    Added dummy wrapper for "a" action type

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -22,7 +22,36 @@ def wrap_action_d_plus_a(env, offset=-8, lb: int = 0, ub: int = 16):
 
     def wrapped_step(self, action):
         states = [self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities]
-        modified_action = np.array([state['latest_demand'] for state in states]) + action + offset
+        modified_action = np.array([state["latest_demand"] for state in states]) + action + offset
+
+        return self.wrappee_step(np.clip(modified_action, lb, ub))
+
+    env.step = types.MethodType(wrapped_step, env)
+
+    return env
+
+
+def wrap_action_a(env, offset=-8, lb: int = 0, ub: int = 16):
+    """A wrapper function that modify the step() function of the given environment.
+
+    After wrapping the environment, the action values provided to the step() function will be interpreted as the 'a' in
+    'd+a' order rule, where 'd' is the latest demand and 'a' is the action value. The quantity d+a will be ordered
+    instead of a.
+
+    Args:
+        env:
+        offset:
+        lb:
+        ub:
+    Returns:
+        A wrapped environment with modified step() function.
+    """
+    env.wrappee_step = env.step
+
+    def wrapped_step(self, action):
+        states = [self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities]
+        # modified_action = np.array([state['latest_demand'] for state in states]) + action + offset
+        modified_action = np.array([action])
 
         return self.wrappee_step(np.clip(modified_action, lb, ub))
 
@@ -42,5 +71,3 @@ def wrap_action_d_plus_a(env, offset=-8, lb: int = 0, ub: int = 16):
 #         raise TypeError(f'states must be type of list of dict or numpy.ndarray')
 #
 #     return np.maximum(modified_action, 0)
-
-
