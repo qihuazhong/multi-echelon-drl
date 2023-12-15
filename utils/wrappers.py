92cf26b190cfc4f734d5b9054a9debaf40b9fdd5
Qihua Zhong <zhongqihua@gmail.com>
2023-12-15 18:42:10 +0800

    Added action_multiplier to support bucketized action env

--- utils/wrappers.py
+++ utils/wrappers.py
@@ -3,7 +3,9 @@ import types
 import numpy as np
 
 
-def wrap_action_d_plus_a(env, offset=-8, lb: int = 0, ub: int = 16):
+def wrap_action_d_plus_a(
+    env, offset=-8, action_multiplier: int = 1, lb: int = 0, ub: int = 16
+):
     """A wrapper function that modify the step() function of the given environment.
 
     After wrapping the environment, the action values provided to the step() function will be interpreted as the 'a' in
@@ -21,8 +23,14 @@ def wrap_action_d_plus_a(env, offset=-8, lb: int = 0, ub: int = 16):
     env.wrappee_step = env.step
 
     def wrapped_step(self, action):
-        states = [self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities]
-        modified_action = np.array([state["latest_demand"] for state in states]) + action + offset
+        states = [
+            self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities
+        ]
+        modified_action = (
+            np.array([state["latest_demand"] for state in states])
+            + action * action_multiplier
+            + offset
+        )
 
         return self.wrappee_step(np.clip(modified_action, lb, ub))
 
@@ -31,7 +39,9 @@ def wrap_action_d_plus_a(env, offset=-8, lb: int = 0, ub: int = 16):
     return env
 
 
-def wrap_action_a(env, offset=-8, lb: int = 0, ub: int = 16):
+def wrap_action_a(
+    env, offset=-8, action_multiplier: int = 1, lb: int = 0, ub: int = 16
+):
     """A wrapper function that modify the step() function of the given environment.
 
     After wrapping the environment, the action values provided to the step() function will be interpreted as the 'a' in
@@ -49,9 +59,11 @@ def wrap_action_a(env, offset=-8, lb: int = 0, ub: int = 16):
     env.wrappee_step = env.step
 
     def wrapped_step(self, action):
-        states = [self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities]
+        states = [
+            self.sn.get_state(facility) for facility in self.sn.agent_managed_facilities
+        ]
         # modified_action = np.array([state['latest_demand'] for state in states]) + action + offset
-        modified_action = np.array(action).reshape(-1)
+        modified_action = np.array(action * action_multiplier).reshape(-1)
         return self.wrappee_step(np.clip(modified_action, lb, ub))
 
     env.step = types.MethodType(wrapped_step, env)
